42d7fdb09a037262532128b91ba5dda24ed6f129
unknown <forkan.saadaoui@gmail.com>
2024-01-29 09:09:30 +0100

    refactoring

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,6 +5,46 @@ function Header() {
   const mobileNav = () => {
     setOpen(!open);
   };
+
+  const Search = () => {
+    return (
+      <div className="relative w-4/5 sm:w-1/2">
+        <input
+          placeholder="Search Products ...."
+          type="text"
+          className="p-2 px-4 text-left text-zinc-600 bg-zinc-50 border border-zinc-600 
+          placeholder:text-xs placeholder:text-left focus:outline-none w-full rounded-xl"
+        />
+        <button
+          type="submit"
+          className="absolute top-0 right-0 bg-emerald-500 w-9 h-full rounded-r-xl border border-zinc-600"
+        >
+          <i className="fa-solid fa-magnifying-glass text-sm text-zinc-100"></i>
+        </button>
+      </div>
+    );
+  };
+
+  const Links = () => {
+    return (
+      <div className="flex items-center md:flex-row flex-col my-2">
+        <div
+          className="text-emerald-50 md:hover:text-emerald-400 hover:font-bold hover:cursor-pointer duration-200
+            text-xl md:text-md hover:text-emerald-100 my-3"
+        >
+          <i className="fa-solid fa-cart-shopping text-sm mr-0.5"></i>
+          <span>Cart</span>
+        </div>
+        <div
+          className="mx-4 md:text-emerald-300 hover:font-bold hover:cursor-pointer duration-200
+          text-emerald-100 text-xl md:text-md my-3"
+        >
+          <i className="fa-solid fa-user text-sm mr-0.5"></i>
+          <span>Sign In</span>
+        </div>
+      </div>
+    );
+  };
   return (
     <>
       <div className="fixed top-0 left-0 w-full z-10 flex items-center justify-around bg-zinc-600 py-2 ">
@@ -12,30 +52,8 @@ function Header() {
           MERN Shop
         </div>
         <div className="hidden md:flex justify-around items-center w-[70%]">
-          <div className="relative">
-            <input
-              placeholder="Search Products ...."
-              type="text"
-              className="p-2 px-4 text-left text-zinc-600 bg-zinc-50 border border-zinc-600 
-                    placeholder:text-xs placeholder:text-left focus:outline-none w-full rounded-xl"
-            />
-            <button
-              type="submit"
-              className="absolute top-0 right-0 bg-emerald-500 w-9 h-full rounded-r-xl border border-zinc-600"
-            >
-              <i className="fa-solid fa-magnifying-glass text-sm text-zinc-100"></i>
-            </button>
-          </div>
-          <div className="flex items-center">
-            <div className="text-emerald-50 hover:text-emerald-400 hover:font-bold hover:cursor-pointer duration-200">
-              <i className="fa-solid fa-cart-shopping text-sm mr-0.5"></i>
-              <span>Cart</span>
-            </div>
-            <div className="mx-4 text-emerald-300 hover:font-bold hover:cursor-pointer duration-200">
-              <i className="fa-solid fa-user text-sm mr-0.5"></i>
-              <span>Sign In</span>
-            </div>
-          </div>
+          <Search/>
+          <Links/>
         </div>
         <div
           className="md:hidden cursor-pointer mr-4 text-zinc-100"
@@ -49,35 +67,12 @@ function Header() {
         </div>
       </div>
 
-      <div
-        className={`absolute left-0  bg-zinc-400 w-full h-full flex flex-col
+      <div className={`absolute left-0  bg-zinc-400 w-full h-full flex flex-col
                     items-center justify-center transition-all duration-500 ease-in
                     ${open ? "top-0" : "top-[-100%]"} md:top-[-100%]`}
       >
-        <div className="relative w-4/5 sm:w-1/2">
-          <input
-            placeholder="Search Products ...."
-            type="text"
-            className="p-2 px-4 text-left text-zinc-600 bg-zinc-50 border border-zinc-600 
-                    placeholder:text-md placeholder:text-left focus:outline-none w-full rounded-xl"
-          />
-          <button
-            type="submit"
-            className="absolute top-0 right-0 bg-emerald-500 w-9 h-full rounded-r-xl border border-zinc-600"
-          >
-            <i className="fa-solid fa-magnifying-glass text-sm text-zinc-100"></i>
-          </button>
-        </div>
-        <div className="flex flex-col items-center my-2">
-          <div className="text-emerald-50 text-xl hover:text-emerald-100 hover:font-bold hover:cursor-pointer duration-200 my-3">
-            <i className="fa-solid fa-cart-shopping text-sm mr-2"></i>
-            <span>Cart</span>
-          </div>
-          <div className="text-emerald-100 text-xl hover:font-bold hover:cursor-pointer duration-200 my-3">
-            <i className="fa-solid fa-user text-sm mr-2"></i>
-            <span>Sign In</span>
-          </div>
-        </div>
+        <Search/>
+        <Links/>
       </div>
     </>
   );
