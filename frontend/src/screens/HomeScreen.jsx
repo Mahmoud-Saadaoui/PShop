b1f666baece239bd5a6952a58e3c4afc73d92aba
unknown <forkan.saadaoui@gmail.com>
2024-02-14 23:45:02 +0100

    get products

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,29 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios'
 import Product from '../components/Product'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
 
 function HomeScreen() {
-  const [products, setProducts] = useState([]);
+  const { data: products, isLoading, error, isError } = useGetProductsQuery();
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      setProducts(data)
-    }
-    fetchProducts()
-  }, []);
   return (
-    <div className='mt-20 mx-6 md:mt-24'>
-      <h1 className='font-bold text-xl font-sans text-slate-700'>
-        Latest Products
-      </h1>
-      <div className="mt-6 xs:flex flex-wrap justify-center">
-      {
-        products.map((product, index) => (
-          <Product product={product} key={index}/>
-        ))
-      }
-      </div>
-    </div>
-  )
+    <>
+      {isLoading ? (
+        <p>loading ...</p>
+      ) : isError ? (
+        <p>{error?.data?.message || error?.error}</p>
+      ) : (
+        <div className="mt-20 mx-6 md:mt-24">
+          <h1 className="font-bold text-xl font-sans text-slate-700">
+            Latest Products
+          </h1>
+          <div className="mt-6 xs:flex flex-wrap justify-center">
+            {products.map((product, index) => (
+              <Product product={product} key={index} />
+            ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default HomeScreen
