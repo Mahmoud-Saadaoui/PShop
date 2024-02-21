0a386ca743b25a2f360530c2053ce40bcd5b54b3
unknown <forkan.saadaoui@gmail.com>
2024-02-21 16:40:48 +0100

    Register User & User Profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 /**
  * desc   Auth user & get token
@@ -11,18 +12,9 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-    //  Set JWT as http-only cookie
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
-      maxAge: 30 * 24 * 60 * 60 * 1000, //30days
-    });
+    generateToken(res, user._id)
 
-    res.json({
+    res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -39,7 +31,30 @@ const authUser = asyncHandler(async (req, res) => {
  * access Public
  */
 const registerUser = asyncHandler(async (req, res) => {
-  res.send("register user");
+  const { name, email, password } = req.body;
+  const userExist = await User.findOne({ email })
+  if (userExist) {
+    res.status(400).json({ message: 'User Already Exist' })
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    generateToken(res, user._id)
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(400).json({ message: "Invalid User Data" })
+  }
 });
 
 /**
@@ -62,7 +77,18 @@ const logoutUser = asyncHandler(async (req, res) => {
  * access Private
  */
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("get user profile");
+  const user = await User.findOne( req.user._id )
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(400).json({ message: "User Not Found" })
+  }
 });
 
 /**
@@ -71,7 +97,26 @@ const getUserProfile = asyncHandler(async (req, res) => {
  * access Private
  */
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("update user profile");
+  const user = await User.findOne(req.user._id)
+
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updateUser = await user.save()
+
+    res.status(200).json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin
+    })
+  } else {
+    res.status(400).json({ message: "User Not Found" })
+  }
 });
 
 /**
