d164e64da2db7afab010e719da0283bc381e556d
unknown <forkan.saadaoui@gmail.com>
2024-02-22 21:40:43 +0100

    login screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import CartScreen from './screens/CartScreen'
-import SignIn from './screens/SignIn'
 import ProductScreen from './screens/ProductScreen'
+import LoginScreen from './screens/LoginScreen'
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path='/' element={<HomeScreen/>}/>
         <Route path='/product/:id' element={<ProductScreen/>}/>
         <Route path='/cart' element={<CartScreen/>}/>
-        <Route path='/login' element={<SignIn/>}/>
+        <Route path='/login' element={<LoginScreen/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
